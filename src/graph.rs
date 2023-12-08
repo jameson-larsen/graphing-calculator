32d0b5a2fdcd477259102725770b6aa4c38eb706
Jameson Larsen <jamesonlarsen@utah.gov>
2023-12-07 20:15:40 -0700

    remove magic number for line width

--- src/graph.rs
+++ src/graph.rs
@@ -39,8 +39,8 @@ pub fn draw_initial_grid(rendering_context: &CanvasRenderingContext2d, x_start: 
     rendering_context.set_stroke_style(&"gray".into());
     let start = (x_start + 1.0).floor() as i32;
     let end = x_end.ceil() as i32;
-    let line_size: f64 = 1e-3 * ((end - start) / 2) as f64;
-    let main_axis_size = 3e-3 * ((end - start) / 2) as f64;
+    let line_size = 0.5 / (rendering_context.canvas().unwrap().width() as f64 / (x_end - x_start));
+    let main_axis_size = 0.8 / (rendering_context.canvas().unwrap().width() as f64 / (x_end - x_start));
     rendering_context.begin_path();
     rendering_context.set_line_width(line_size);
     for i in (start..end).step_by(step_size) {
@@ -67,7 +67,7 @@ pub fn draw_initial_grid(rendering_context: &CanvasRenderingContext2d, x_start: 
 
 pub fn draw_function_graph(rendering_context: &CanvasRenderingContext2d, calculator: &mut Calculator, cache: &mut Vec<(f64, Option<f64>)>, x_start: f64, x_end: f64, y_start: f64, y_end: f64, step_size: f64, idx: usize) {
     rendering_context.set_stroke_style(&COLORS[idx % COLORS.len()].into());
-    let line_size: f64 = 3e-3 * ((x_end - x_start) / 2.0) as f64;
+    let line_size = 0.9 / (rendering_context.canvas().unwrap().width() as f64 / (x_end - x_start));
     rendering_context.set_line_width(line_size);
     let mut x = x_start;
     let mut y;
@@ -129,7 +129,7 @@ pub fn draw_function_graph(rendering_context: &CanvasRenderingContext2d, calcula
 
 pub fn draw_function_graph_from_cache(rendering_context: &CanvasRenderingContext2d, cache: &Vec<(f64, Option<f64>)>, x_start: f64, x_end: f64, y_start: f64, y_end: f64, step_size: f64, idx: usize) {
     rendering_context.set_stroke_style(&COLORS[idx % COLORS.len()].into());
-    let line_size: f64 = 3e-3 * ((x_end - x_start) / 2.0) as f64;
+    let line_size = 0.9 / (rendering_context.canvas().unwrap().width() as f64 / (x_end - x_start));
     rendering_context.set_line_width(line_size);
     let mut i = ((x_start - cache[0].0) / step_size).floor() as usize;
     let mut x = cache[i].0;
